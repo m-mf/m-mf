d16d98ecdf7fe88d8d5ead893da10218af5e7962
keneanung <keneanung@googlemail.com>
2015-09-06 21:02:43 +0200

    Add files and do changes needed for travis
    More changes needed are the encrypted keys.

--- precommit.lua
+++ precommit.lua
@@ -10,27 +10,26 @@
 
 -- docs
 -- update version in doc
-sassert(io.input("/home/vadi/Games/Mudlet/mm/generate.lua"))
-t = io.read("*all")
-systemversion = string.match(t, [[version = "(.-)"]])
+local args = {...}
+local systemversion = args[1]
 
-io.input("/home/vadi/Games/Mudlet/mm/doc/conf.py")
+io.input("doc/conf.py")
 t = io.read("*all")
 t = string.gsub(t, [[version = '.-']], string.format("version = '%s'", systemversion))
 t = string.gsub(t, [[release = '.-']], string.format("release = '%s'", systemversion))
-io.output("/home/vadi/Games/Mudlet/mm/doc/conf.py")
+io.output("doc/conf.py")
 io.write(t)
 
 -- regen docs
-os.execute([[cd ~/Games/Mudlet/mm/doc && sphinx-build -j 4 -b html -d _build/doctrees   . _build/html]])
+os.execute([[cd doc && sphinx-build -b html -d _build/doctrees   . _build/html]])
 
 -- update ndb cache listing
-package.path = package.path .. ";doc/?.lua"
+package.path = package.path .. ";./doc/?.lua"
 gl     = require"parse_glossary"
 pretty = require"pl.pretty"
 
 local data = gl.striptrailinglines(gl.readfile("doc/namedb.rst"))
 
-local f = io.open([[/home/vadi/Games/Mudlet/m&m template/ndb-help.lua]], "w+")
+local f = io.open([[m&m template/ndb-help.lua]], "w+")
 f:write(pretty.write(data))
 f:close()
