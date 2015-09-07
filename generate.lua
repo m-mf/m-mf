c1c278512d9f7dba64eb91ccdfdf36245570a04c
siddister <mike.p.manning@gmail.com>
2015-09-07 19:29:15 -0400

    reverting version back to 1

--- generate.lua
+++ generate.lua
@@ -38,7 +38,7 @@ local args = lapp [[
 
 local name    = args.name
 local release = not args.debug
-local version = "1s"
+local version = "1"
 local doall   = args.release
 local own     = args.own
 local defaultaddons = {
